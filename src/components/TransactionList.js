3f69e80f7c8fc2f3674a663ef222ee0a69f80f84
Pathompong Sirisawat <6710110239@psu.ac.th>
2024-12-16 23:52:28 +0700

    Add edit button to Trasaction.js

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,55 +1,70 @@
 import React from "react"
 import { Button, Table, Space, Tag, Popconfirm, Modal } from "antd"
-import { DeleteOutlined, BugOutlined } from '@ant-design/icons';
+import { DeleteOutlined, BugOutlined, EditOutlined } from '@ant-design/icons';
 import dayjs from "dayjs";
 
 export default function TransactionList(props) {
 
   const columns = [
-    { title: "Date-Time", 
-      dataIndex: "action_datetime", 
+    {
+      title: "Date-Time",
+      dataIndex: "action_datetime",
       key: "action_datetime",
       render: (_, record) => dayjs(record.action_datetime).format("DD/MM/YYYY - HH:mm")
     },
-    { 
+    {
       title: "Type", dataIndex: "type", key: "type", render: (_, record) => (
         <Tag color={record.type === "income" ? 'green' : 'red'}>{record.type}</Tag>
-      ) 
+      )
     },
     { title: "Amount", dataIndex: "amount", key: "amount" },
     { title: "Note", dataIndex: "note", key: "note" },
     {
       title: "Action", key: "action", render: (_, record) => (
         <Space size="middle">
-          
+
+          <Button
+            type="primary"
+            shape="square"
+            icon={<EditOutlined />}
+            style={{ backgroundColor: '#878787' }}
+            onClick={() => {
+              Modal.confirm({
+                title: "Edit the transaction"
+              })
+            }}>
+            Edit
+          </Button>
+        
+
           <Popconfirm
             title="Delete the transaction"
             description="Are you sure to delete this transaction?"
             onConfirm={() => props.onRowDeleted(record.id)}
           >
-            <Button danger 
-              type="primary" 
-              shape="circle" 
+            <Button danger
+              type="primary"
+              shape="circle"
               icon={<DeleteOutlined />} />
           </Popconfirm>
-          <Button 
-            type="primary" 
-            shape="circle" 
-            icon={<BugOutlined/>} 
+          <Button
+            type="primary"
+            shape="circle"
+            icon={<BugOutlined />}
             onClick={() => {
               Modal.info({
                 title: "Debug",
                 content: JSON.stringify(record)
               })
-            }}/>
-        </Space>
-      ), 
+            }} />
+        </Space >
+      ),
     },
   ]
 
   return (
     <>
-    <Table columns={columns} dataSource={props.data}/>
+      <Table columns={columns} dataSource={props.data} />
     </>
   )
 }
